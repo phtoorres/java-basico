1fa124eb083d4e5babb15a251827121f6bdfc94c
paulotorres <paulohrat@gmail.com>
2023-09-05 13:19:45 -0300

    switch case

---
+++ controle-de-fluxos/src/PlanoOperadora.java
@@ -0,0 +1,19 @@
+public class PlanoOperadora {
+    public static void main(String[] args){
+        String plano = "T";
+
+        switch (plano){
+            case "T":{
+                System.out.println("5gb YouTube");
+            
+            }
+            case "M":{
+                System.out.println("Whatsapp e instagram grátis");
+
+            }
+            case "B":{
+                System.out.println("100 minutos de ligação");
+            }
+        }
+    }
+}
